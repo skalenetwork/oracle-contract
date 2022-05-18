703092eecea68598d59de5e84782efcf37700b72
Artem Payvin <payvinartem@gmail.com>
2022-05-18 15:15:00 +0100

    Add version and test deploy

---
+++ scripts/test_deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+npx ganache-cli --gasLimit 8000000 --quiet &
+
+MODE=FIXED npx hardhat run migrations/deploy.ts
+MODE=UPGRADEABLE npx hardhat run migrations/deploy.ts
+
+npx kill-port 8545
+
+while true
+do
+    if ! netstat -t -u -l -p -n | grep 8545
+    then
+        break
+    fi
+    echo "Port 8545 is busy"
+    sleep 1
+done
